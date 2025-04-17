2116ad49073ab6e827c83db14e54c7c3182c8e99
GauravMG <gauravmittal17396@gmail.com>
2025-04-17 23:27:55 +0530

    Revert "initial commit"
    This reverts commit 1e5132510072b3c1767b0c507809f91c677e8b01.

--- src/helpers/generators.ts
+++
@@ -1,5 +0,0 @@
-export function generateOTP(length: number = 6): string {
-	const min = Math.pow(10, length - 1) // Smallest number with the desired length
-	const max = Math.pow(10, length) - 1 // Largest number with the desired length
-	return Math.floor(min + Math.random() * (max - min + 1)).toString()
-}
--- src/types/is-json.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "is-json" {
-	export default function isJSON(data: string | object): boolean
-}
